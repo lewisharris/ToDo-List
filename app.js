613f3903dbab63b258d16af80f239293d184dc37
Lewis Harris <44181381+lewisharris@users.noreply.github.com>
2020-01-22 23:54:19 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,76 @@
+getId = (x) => {return document.getElementById(x)}; // GetElementById shortcut function
+
+/*
+const add = getId('inputform');
+const textinput = getId('textinput');
+const ul = getId('mainListBody');
+
+//add new item to list
+add.addEventListener('submit',function(){
+    event.preventDefault();
+    if(textinput.value != ''){
+        let li = document.createElement('li');
+        li.appendChild(document.createTextNode(textinput.value));
+        li.setAttribute('class','listItem');
+        ul.appendChild(li);
+        textinput.innerHTML == '';
+    }
+
+});
+
+//clear selected items from list
+const deleteSelected = getId('clearselected');
+
+deleteSelected.addEventListener('click',function(){
+    for(i=0;i<ul.children.length; i++){
+        if(ul.children[i].className != 'checked'){
+        ul.removeChild();
+        }
+    }
+    });
+
+
+//clear all items from list
+const clear = getId('clear');
+clear.addEventListener('click',function(){
+    ul.innerHTML = '';
+    
+})
+
+*/
+
+const text = getId('textinput');
+const iD = new Date().getTime();
+const add = getId('add');
+const list = getId('list');
+const clearAll = getId('clearall')
+const clearSelected = getId('clearselected');
+
+
+//Add item to list
+add.addEventListener('click',() => {
+    event.preventDefault();
+    if(text.value != ''){
+        list.insertAdjacentHTML("beforeend",`<li class="item" id="${iD}">${text.value}</li>`);
+        //text.value = '';
+        //text.focus();
+    }
+});
+
+//Check and delete items
+list.addEventListener('click',() =>{
+    const e = event.target;
+    e.parentNode.classList.toggle('checked');
+    if(e.className = 'checked'){
+        e.style.opacity = '0';
+        setTimeout(() => { e.parentNode.removeChild(e)},800);
+    }
+})
+
+// Clear list
+clearAll.addEventListener('click', () => { list.innerHTML = ''});
+
+
+
+
+
