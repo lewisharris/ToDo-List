0e9943262d5523e1bebf0ec6f18776a088d981fd
Lewis Harris <44181381+lewisharris@users.noreply.github.com>
2020-01-23 14:37:10 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -1,44 +1,5 @@
 getId = (x) => {return document.getElementById(x)}; // GetElementById shortcut function
 
-/*
-const add = getId('inputform');
-const textinput = getId('textinput');
-const ul = getId('mainListBody');
-
-//add new item to list
-add.addEventListener('submit',function(){
-    event.preventDefault();
-    if(textinput.value != ''){
-        let li = document.createElement('li');
-        li.appendChild(document.createTextNode(textinput.value));
-        li.setAttribute('class','listItem');
-        ul.appendChild(li);
-        textinput.innerHTML == '';
-    }
-
-});
-
-//clear selected items from list
-const deleteSelected = getId('clearselected');
-
-deleteSelected.addEventListener('click',function(){
-    for(i=0;i<ul.children.length; i++){
-        if(ul.children[i].className != 'checked'){
-        ul.removeChild();
-        }
-    }
-    });
-
-
-//clear all items from list
-const clear = getId('clear');
-clear.addEventListener('click',function(){
-    ul.innerHTML = '';
-    
-})
-
-*/
-
 const text = getId('textinput');
 const iD = new Date().getTime();
 const add = getId('add');
@@ -51,10 +12,12 @@ const clearSelected = getId('clearselected');
 add.addEventListener('click',() => {
     event.preventDefault();
     if(text.value != ''){
-        list.insertAdjacentHTML("beforeend",`<li class="item" id="${iD}"><input class="checkbox" type="checkbox" value="false">${text.value}</li>`);
-        //text.value = '';
-        //text.focus();
+        list.insertAdjacentHTML("beforeend",`<li class="item" id="${iD}"><span>${text.value}</span><input class="checkbox" type="checkbox" value="false"></li>`);
+        text.value = '';
+        text.focus();
     }
+    else{
+        text.focus();}
 });
 
 //Check and delete items
